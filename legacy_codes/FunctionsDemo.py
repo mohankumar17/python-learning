c1f375b3d944a29e6cfe495f19a794a93e74d337
mohankumar17 <kothalamohankumar17@gmail.com>
2024-03-08 12:54:12 +0530

    Removed some legacy codes and added oops codes

--- legacy_codes/FunctionsDemo.py
+++
@@ -1,8 +0,0 @@
-def isEven(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
-
-x = int(input("Enter a number: "))
-print(isEven(x))
--- legacy_codes/Pandas.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame([(1, "Paul", 32), (2, "Tina", 36), (3, "John", 26)], columns=["ID", "NAME", "AGE"])
-#print(df)
-
-print(df["NAME"][2])
